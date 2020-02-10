1286fecb24b40be27bcd9ca2294dd75507246fd1
shohanurvu@gmail.com <shohanurvu@gmail.com>
2020-02-10 16:59:08 +0600

    array_programm_final

--- midvalueofanArray.cpp
+++ midvalueofanArray.cpp
@@ -1,13 +1,10 @@
 #include<iostream>
 using namespace std;
-
 int  main()
 {
     int arr[]={1,2,3,4,5,6,7,8};
     int len;
     len=sizeof(arr)/sizeof(arr[0]);
     cout<<len-1;
-
     return 0;
-
 }
