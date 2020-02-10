875ef9b5ba588c1ae72c33174cc948cc22830bfe
shohanurvu@gmail.com <shohanurvu@gmail.com>
2020-02-10 17:35:50 +0600

    nothing so important

--- linersearch.cpp
+++ linersearch.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 
 using namespace std;
+
 int linerseach(int values[], int target, int n)
 {
     int i;
@@ -11,18 +12,15 @@ int linerseach(int values[], int target, int n)
             return i;
         }
     }
-
     return -1;
 }
 
 int main()
 {
-
     int elemnts[] = {1, 5, 65, 2, 7, 8, 34};
     int findvalue;
     int length;
     length = sizeof(elemnts) / sizeof(elemnts[0]);
-
     cout << "Enter the values that you want to search:=";
     cin >> findvalue;
     cout << linerseach(elemnts, findvalue, length);
