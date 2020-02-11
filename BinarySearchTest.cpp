1653b8b8fa532ba7194ad0cceb3cef8c6fe0327b
shohanurvu@gmail.com <shohanurvu@gmail.com>
2020-02-11 16:41:11 +0600

    commit_again

---
+++ BinarySearchTest.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+int BinarySearch(int values[],int lengthOfArray,int target)
+{
+    int left,right,mid;
+    left=0;
+    mid=0;
+    right=lengthOfArray-1;
+    
+    
+
+    while (left<=right)
+    {
+        mid=(left+right)/2;
+    
+        if(values[mid]<target)
+        {
+            left=mid+1;
+        }
+        else if (values[mid]>target)
+        {
+            right=mid-1;
+        }
+        else
+        {
+            return mid;
+        }
+
+        
+        
+
+    }
+    return -1;
+    
+
+}
+
+
+int main()
+{
+    int arr[]={0,1896,24568,3286,4896,5496,68569,76984,84568,385659};
+    int length,find,result;
+    length=sizeof(arr)/sizeof(arr[0]);
+    cin>>find;
+    result=BinarySearch(arr,length,find);
+    cout<<result<<endl;
+
+
+
+
+
+}
--- linersearchpp.cpp
+++ linersearchpp.cpp
@@ -5,11 +5,11 @@ using namespace std;
 int linerSearch(int values[], int target, int lenghtofArray)
 {
     int i;
-    for(i=0;i<lenghtofArray;i++)
+    for (i = 0; i < lenghtofArray; i++)
     {
-        if(values[i]==target)
+        if (values[i] == target)
         {
-            return i+1;
+            return i + 1;
         }
     }
     return -1;
@@ -17,16 +17,14 @@ int linerSearch(int values[], int target, int lenghtofArray)
 int main()
 {
 
-    int arr[]={23,66,535,34,2,6,8};
-    int l,f,result,t=0;
-    l=sizeof(arr)/sizeof(arr[0]);
+    int arr[] = {23, 66, 535, 34, 2, 6, 8};
+    int l, f, result, t = 0;
+    l = sizeof(arr) / sizeof(arr[0]);
 
-    while (t==0)
+    while (t == 0)
     {
-        cin>>f;
-    result =linerSearch(arr,f,l);
-    cout<<result<<endl;
+        cin >> f;
+        result = linerSearch(arr, f, l);
+        cout << result << endl;
     }
-    
-    
 }
