02b485c473d5f7fba12943f00bd80943fa1834a0
shohanurvu@gmail.com <shohanurvu@gmail.com>
2020-02-17 17:33:21 +0600

    while_loop_itreation

---
+++ whilelooptoprintreverse.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+    int i,n;
+    i = 0;
+    cout << "Enter the value of Itreation = ";
+    cin >> n;
+    while (i<=n)
+    {
+        cout << i << "\n";
+        i++;
+        }
+    
+}
